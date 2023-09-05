5aa93ecdd61867fe66b8c5df3167d317cf4c4113
Dani-Ideas <daniideas0@gamil.com>
2023-09-05 07:13:33 -0600

    compleate class listaSimple

--- src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimple.java
+++ src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimple.java
@@ -9,10 +9,32 @@ public class ListaSimple<E> {
 		header=cola=null;
 	}
 	
-	public void agraegarEnCabeza(E dato) {
-		header = new Nodo<E>(dato, header);
+	public void agraegarEnCabeza(E inputData) {
+		header = new Nodo<E>(inputData, header);
 		if (cola == null) {
-			
+			cola= header;
+		}
+		longitud++;
+	}
+	public void agregarEnCola(E inputData) {
+		if (header==null) {
+			header= cola= new Nodo<E>(inputData);
+		}else {
+			cola.setNext(new Nodo<E>(inputData));
+			cola = cola.getNext();
+		}
+		longitud++;
+	}
+	public void imprimirElemetos() {
+		for (Nodo<E> temporal = header; temporal !=null; temporal.getNext()) {
+			System.out.println(temporal.getData());
 		}
 	}
+	public E obtenerCabeza() {
+		return header.getData();
+	}
+	public E obtenerCola() {
+		return cola.getData();
+	}
+	
 }
