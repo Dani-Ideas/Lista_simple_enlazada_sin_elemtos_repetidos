5e8e057ca4fdace4271dcc86097101672203e831
Dani-Ideas <daniideas0@gamil.com>
2023-09-11 19:30:07 -0600

    completando espesificaciones de guardar los elentos borrados y mostrarlos en una lista alternativa

--- src/Herramintas/Herramientas.java
+++ src/Herramintas/Herramientas.java
@@ -9,19 +9,19 @@ public class Herramientas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ListaSimpleSinRepetir<Integer> lista1=new ListaSimpleSinRepetir<Integer>();
-		//ListaSimpleSinRepetir<Integer> lista2=new ListaSimpleSinRepetir<Integer>();
+		ListaSimpleSinRepetir<Integer> lista2=new ListaSimpleSinRepetir<Integer>();
 		Random numeberRandomGenerate= new Random();
 		
 		for (int i=0; i<10; i++) {
-			int numberRandom= numeberRandomGenerate.nextInt(50,121);
+			int numberRandom= numeberRandomGenerate.nextInt(1501);
 			//lista2.agregarEnCabeza(numberRandom);
-			lista1.agregarEnCola(numberRandom);
+			lista1.agregarEnCola(numberRandom,lista2);
 		}
 		
 		lista1.imprimirElemetos();
 		System.out.println("--------------------------");
-		lista1.agregarEnCola(152);
-		lista1.agregarEnCola(152);
+		lista1.agregarEnCola(152,lista2);
+		lista1.agregarEnCola(152,lista2);
 		lista1.imprimirElemetos();
 	}
 }
--- src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimpleSinRepetir.java
+++ src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimpleSinRepetir.java
@@ -1,15 +1,19 @@
 package fes.aragon.utilerias.dinamicas.listasimple;
 
 public class ListaSimpleSinRepetir<E> extends ListaSimple<E>{
-
-	public void agregarEnCabeza(E inputData) {
+	protected Nodo<E> temporal; 
+	public void agregarEnCabeza(E inputData,ListaSimpleSinRepetir<E> listErrace) {
 		boolean es_Duplicado= super.eliminar(inputData); 
 		if (!es_Duplicado) 
 			super.agregarEnCabeza(inputData);
+		else 
+			listErrace.agregarEnCola(inputData);
 	}
-	public void agregarEnCola(E inputData) {
+	public void agregarEnCola(E inputData,ListaSimpleSinRepetir<E> listErrace) {
 		boolean es_Duplicado= super.eliminar(inputData); 
 		if (!es_Duplicado)
 			super.agregarEnCola(inputData);
+		else 
+			listErrace.agregarEnCola(inputData);
 	}
 }
